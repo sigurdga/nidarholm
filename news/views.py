81d6c20f15c7312363c5d455c49d2985e205ff1e
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-10-03 14:19:40 +0200

    convert news list to use generic view
    generic views passes request_context automatically
    request_context gives user variable in templates
    this commit also added login/logout based on user variable

--- news/views.py
+++ news/views.py
@@ -1,32 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
-from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.template.defaultfilters import slugify
 from django.conf import settings
 from news.models import Story
 from news.forms import StoryForm
 from pages.models import FlatPage
+from django.views.generic import list_detail
 
-def story_list(request):
-    story_list = Story.objects.filter(parent=None)
-    paginator = Paginator(story_list, 25)
-
-    try:
-        page = int(request.GET.get('page', '1'))
-    except ValueError:
-        page = 1
-
-    try:
-        stories = paginator.page(page)
-    except (EmptyPage, InvalidPage):
-        stories = paginator.page(paginator.num_pages)
-    
+def story_list(request, page=1):    
     if page == 1:
         infopage = get_object_or_404(FlatPage, url__exact='/', sites__id__exact=settings.SITE_ID)
     else:
         infopage = None
 
-    return render_to_response('news/main.html', {'stories': stories, 'infopage': infopage})
+    return list_detail.object_list(request,
+                                   queryset=Story.objects.filter(parent=None),
+                                   page=page,
+                                   template_name='news/main.html',
+                                   extra_context={'infopage': infopage})
 
 def story(request, slug):
     debate = get_object_or_404(Story, slug=slug)
