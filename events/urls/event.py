b74c47fb7c3007b063c36b6bb81206ae65e18848
Sigurd Gartmann <sigurd.gartmann@sbexp.com>
2010-10-07 20:57:44 +0200

    extend generic views for events to use permission checking

--- events/urls/event.py
+++ events/urls/event.py
@@ -1,54 +1,12 @@
-from events.views import new_event, edit_event
 from django.conf.urls.defaults import patterns
-from django.views.generic.list_detail import object_list
-from django.views.generic.date_based import archive_year, archive_month, archive_day, object_detail
+from events.views import *
 
-from datetime import date
-
-from events.models import Event
-from django.template.context import RequestContext
-from django.core.context_processors import request
-
-upcoming_dict = {
-                 'queryset': Event.objects.filter(start__gt=date.today()),
-                 }
-
-events_year_dict = {
-                   'queryset': Event.objects.all(),
-                   'date_field': 'start',
-                   'make_object_list': True,
-                   'allow_future': True,
-                   }
-
-events_month_dict = {
-                   'queryset': Event.objects.all(),
-                   'date_field': 'start',
-                   'month_format': '%m',
-                   'allow_future': True,
-                   }
-
-events_day_dict = {
-                   'queryset': Event.objects.all(),
-                   'date_field': 'start',
-                   'month_format': '%m',
-                   'day_format': '%d',
-                   'allow_future': True,
-                   }
-
-event_dict = {
-              'queryset': Event.objects.all(),
-              'month_format': '%m',
-              'day_format': '%d',
-              'date_field': 'start',
-              'allow_future': True,
-              }
-
-urlpatterns = patterns('django.views.generic.list_detail',
-    (r'^$', object_list, upcoming_dict, 'events-list'),
+urlpatterns = patterns('',
+    (r'^$', upcoming_events, (), 'events-list'),
     (r'^new$', new_event, (), 'events-new'),
     (r'^(?P<id>\d+)/edit$', edit_event, (), 'events-edit'),
-    (r'^(?P<year>\d{4})$', archive_year, events_year_dict, 'events-year'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})$', archive_month, events_month_dict, 'events-month'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$', archive_day, events_day_dict, 'events-month'),
-    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)$', object_detail, event_dict, 'events-event'),
+    (r'^(?P<year>\d{4})$', event_archive_year, (), 'events-year'),
+    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})$', event_archive_month, (), 'events-month'),
+    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})$', event_archive_day, (), 'events-month'),
+    (r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<slug>[-\w]+)$', event_object_detail, (), 'events-event'),
 )
--- events/views.py
+++ events/views.py
@@ -1,13 +1,45 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render_to_response
-from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from events.models import Event
 from events.forms import EventForm
 from django.template.context import RequestContext
+from django.views.generic import list_detail, date_based
+from datetime import date
 
-def event_details(request, id):
-    event = get_object_or_404(Event, id=id)
-    return render_to_response('events/event.html', {'event': event})
+DATEFIELD = 'start'
+MONTH_FORMAT = '%m'
+
+def upcoming_events(request, page=1):
+
+    return list_detail.object_list(request,
+                                   queryset=Event.objects.for_user(request.user).filter(start__gt=date.today()),
+                                   page=page,
+                                   )
+
+def event_archive_year(request, year):
+    return date_based.archive_year(request, year, Event.objects.for_user(request.user), DATEFIELD,
+                                   make_object_list=True,
+                                   allow_future=True,
+                                   )
+
+def event_archive_month(request, year, month):
+    return date_based.archive_month(request, year, month, Event.objects.for_user(request.user), DATEFIELD,
+                                    month_format=MONTH_FORMAT,
+                                    allow_future=True,
+                                    )
+
+def event_archive_day(request, year, month, day):
+    return date_based.archive_day(request, year, month, day, Event.objects.for_user(request.user), DATEFIELD,
+                                  month_format=MONTH_FORMAT,
+                                  allow_future=True,
+                                  )
+
+def event_object_detail(request, year, month, day, slug):
+    return date_based.object_detail(request, year, month, day, Event.objects.for_user(request.user), DATEFIELD,
+                                    month_format=MONTH_FORMAT,
+                                    slug=slug,
+                                    allow_future=True,
+                                    )
 
 def edit_event(request, id):
     if request.method == 'POST':
