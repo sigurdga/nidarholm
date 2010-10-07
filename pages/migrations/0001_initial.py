1ba5f74c681c4b4a960db62881f68c8403493a90
Sigurd Gartmann <sigurd.gartmann@sbexp.com>
2010-10-07 15:37:05 +0200

    reset initial migrations for pages app

--- pages/migrations/0001_initial.py
+++ pages/migrations/0001_initial.py
@@ -5,37 +5,55 @@ from south.v2 import SchemaMigration
 from django.db import models
 
 class Migration(SchemaMigration):
-
+    
     def forwards(self, orm):
         
         # Adding model 'FlatPage'
-        db.create_table('page', (
-            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('url', self.gf('django.db.models.fields.CharField')(max_length=100, db_index=True)),
+        db.create_table('pages_flatpage', (
             ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('url', self.gf('django.db.models.fields.CharField')(max_length=100, db_index=True)),
             ('content', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('content_markdown', self.gf('django.db.models.fields.TextField')(blank=True)),
             ('pub_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
         ))
         db.send_create_signal('pages', ['FlatPage'])
 
-
+        # Adding M2M table for field sites on 'FlatPage'
+        db.create_table('pages_flatpage_sites', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('flatpage', models.ForeignKey(orm['pages.flatpage'], null=False)),
+            ('site', models.ForeignKey(orm['sites.site'], null=False))
+        ))
+        db.create_unique('pages_flatpage_sites', ['flatpage_id', 'site_id'])
+    
+    
     def backwards(self, orm):
         
         # Deleting model 'FlatPage'
-        db.delete_table('page')
+        db.delete_table('pages_flatpage')
 
-
+        # Removing M2M table for field sites on 'FlatPage'
+        db.delete_table('pages_flatpage_sites')
+    
+    
     models = {
         'pages.flatpage': {
-            'Meta': {'ordering': "('url',)", 'object_name': 'FlatPage', 'db_table': "'page'"},
+            'Meta': {'object_name': 'FlatPage'},
             'content': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'content_markdown': ('django.db.models.fields.TextField', [], {'blank': 'True'}),
             'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'pub_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'sites': ('django.db.models.fields.related.ManyToManyField', [], {'related_name': "'pages'", 'symmetrical': 'False', 'to': "orm['sites.Site']"}),
             'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
             'url': ('django.db.models.fields.CharField', [], {'max_length': '100', 'db_index': 'True'})
+        },
+        'sites.site': {
+            'Meta': {'object_name': 'Site', 'db_table': "'django_site'"},
+            'domain': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
         }
     }
-
+    
     complete_apps = ['pages']
