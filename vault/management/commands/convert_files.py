bf757dee52724a68f8dd72ec7313dd039bb212a5
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-11-22 22:53:57 +0100

    add tags to file list

--- vault/management/commands/convert_files.py
+++ vault/management/commands/convert_files.py
@@ -3,9 +3,9 @@ from django.contrib.auth.models import User, Group
 from tagging.models import Tag
 from vault.models import UploadedFile
 
+from datetime import datetime
 import magic
 import hashlib
-import time
 
 import MySQLdb
 
@@ -22,16 +22,16 @@ class Command(BaseCommand):
             u = User.objects.get(username=row[8].decode('utf-8'))
             g, created = Group.objects.get_or_create(name=row[9].decode('utf-8'))
             f, created = UploadedFile.objects.get_or_create(filename=row[3].decode('utf-8'))
-            timestamp = row[6]
-            f.uploaded_at = timestamp
+            timestamp = datetime.fromtimestamp(float(row[6]))
+            f.uploaded = timestamp
             original_filename = "/srv/www/nidarholm/website/webdocs/innhold/filer/%s.%s" % (hashlib.md5("f_%s" % (row[0],)).hexdigest(), row[2])
-            folder = timestamp[-2:]
-            f.file = folder + '/' + timestamp
+            folder = row[6][-2:]
+            f.file = folder + '/' + row[6]
             #f.content_type = magic.Magic(mime=True).from_file(original_filename)
             f.content_type = m.file(original_filename)
             print original_filename
             print m.file(original_filename)
-            f.tags = ",".join(self.find_tags(cursor, row[7]))
+            f.tags = ",".join(self.find_tags(cursor, int(row[7])))
             if g.name == 'verden':
                 g = None
             f.group = g
@@ -40,13 +40,13 @@ class Command(BaseCommand):
         conn.close()
 
     def find_tags(self, db_cursor, folder_id):
-        db_cursor.execute("select navn, mor, fra from filkatalog where id = %d" % (folder_id,))
-        tag = None
-        for row in db_cursor.fetchall():
-            tag, created = Tag.objects.get_or_create(name=row[0].decode('utf-8'))
-        if row[1]:
-            tags = self.find_tags(db_cursor, row[1])
-        else:
-            tags = []
-        tags.append(tag.name)
+        tags = []
+        if folder_id:
+            db_cursor.execute("select navn, mor, fra from filkatalog where id = %d" % (folder_id,))
+            tag = None
+            for row in db_cursor.fetchall():
+                tag, created = Tag.objects.get_or_create(name=row[0].decode('utf-8'))
+                if row[1]:
+                    tags = self.find_tags(db_cursor, int(row[1]))
+                tags.append(tag.name)
         return tags
