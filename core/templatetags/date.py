4536b924eae7a1c3e9c38abf131eb6d7fd2db201
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-11-24 21:02:10 +0100

    localized versions of date range functions now work

--- core/templatetags/date.py
+++ core/templatetags/date.py
@@ -8,9 +8,12 @@ register = template.Library()
 @register.simple_tag
 def single_datetime(from_date, whole_day=False):
     if whole_day:
-        return date(from_date)
+        return date(from_date, "DATE_FORMAT")
     else:
-        return u"%s %s" % (date(from_date), time(from_date))
+        return u"%s %s" % (
+                    date(from_date, "DATE_FORMAT"),
+                    time(from_date, "TIME_FORMAT"),
+                    )
 
 @register.simple_tag
 def between_datetimes(from_date, to_date, whole_day=False):
@@ -18,20 +21,23 @@ def between_datetimes(from_date, to_date, whole_day=False):
         return single_datetime(from_date, whole_day)
     if whole_day:
         if from_date.day == to_date.day:
-            return date(from_date)
+            return date(from_date, "DATE_FORMAT")
         else:
-            return u"%s – %s" % (date(from_date),
-                    date(to_date),
+            return u"%s – %s" % (
+                    date(from_date, "DATE_FORMAT"),
+                    date(to_date, "DATE_FORMAT"),
                     )
     else:
         if from_date.day == to_date.day:
-            return u"%s %s – %s" % (date(from_date),
-                    time(from_date),
-                    time(to_date),
+            return u"%s %s – %s" % (
+                    date(from_date, "DATE_FORMAT"),
+                    time(from_date, "TIME_FORMAT"),
+                    time(to_date, "TIME_FORMAT"),
                     )
         else:
-            return u"%s %s – %s %s" % (date(from_date),
-                    time(from_date),
-                    date(to_date),
-                    time(to_date),
+            return u"%s %s – %s %s" % (
+                    date(from_date, "DATE_FORMAT"),
+                    time(from_date, "TIME_FORMAT"),
+                    date(to_date, "DATE_FORMAT"),
+                    time(to_date, "TIME_FORMAT"),
                     )
