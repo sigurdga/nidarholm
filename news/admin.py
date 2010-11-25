9beda8a6d03e8e04192e7a389852ba319254af7b
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-11-25 23:42:56 +0100

    remove traces of objectpermissions

--- news/admin.py
+++ news/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from news.models import Story
-from permissions.admin import ObjectPermissionMixin, ObjectPermissionInline
 
-class StoryAdmin(ObjectPermissionMixin, admin.ModelAdmin):
-    #inlines = [ObjectPermissionInline]
+class StoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     fields = ['parent', 'title', 'slug', 'text', 'user', 'group']
 
