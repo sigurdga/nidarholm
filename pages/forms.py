070f78a3e10858d114f322f56961dd9ed43fffee
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-11-28 17:22:38 +0100

    do now show user field

--- pages/forms.py
+++ pages/forms.py
@@ -4,4 +4,4 @@ from pages.models import FlatPage
 class PageForm(ModelForm):
     class Meta:
         model = FlatPage
-        exclude = ('content_html',)
+        exclude = ('content_html', 'user')
