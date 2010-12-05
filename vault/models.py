7ebc792dcb539dfadfaf1917928811067848ab47
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-12-05 15:22:05 +0100

    remove traces of GitFileSystemStorage, that was never implemented

--- vault/models.py
+++ vault/models.py
@@ -41,7 +41,7 @@ def upload_path(instance, filename):
 
 
 class UploadedFile(models.Model):
-    file = models.FileField(upload_to=upload_path, storage=GitFileSystemStorage(location=settings.FILE_SERVE_ROOT + '/originals'))
+    file = models.FileField(upload_to=upload_path, storage=FileSystemStorage(location=settings.FILE_SERVE_ROOT + '/originals'))
     content_type = models.CharField(max_length=15, blank=True)
     filename = models.CharField(max_length=100, blank=True)
     user = models.ForeignKey(User)
