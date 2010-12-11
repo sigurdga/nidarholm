f80aada18237af033a94d39883c678cbca0c0013
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-12-11 14:38:45 +0100

    make event admin more usable

--- events/admin.py
+++ events/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from events.models import Event, EventCategory
 
-admin.site.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    fields = ('title', 'slug', 'location', 'content', 'start', 'end', 'whole_day', 'user', 'group', 'event_serie', 'event_category')
+    list_display = ('title', 'start', 'end', 'location', 'group')
+    list_filter = ('group', 'start')
+    ordering = ('-created',)
+    search_fields = ('title', 'content_html')
+
+admin.site.register(Event, EventAdmin)
 admin.site.register(EventCategory)
