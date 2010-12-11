39b48d66edb55bbaf8504c0f59abdfa6b3616d82
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-12-11 19:01:03 +0100

    make better admin pages

--- organization/admin.py
+++ organization/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from organization.models import GroupCategory, GroupProfile, Role, Membership, SiteProfile
 
 class SiteProfileAdmin(admin.ModelAdmin):
-    fields = ('group', 'site', 'contact_text')
+    fields = ('user', 'group', 'site', 'contact_text')
     list_display = ('site', 'group')
 admin.site.register(SiteProfile, SiteProfileAdmin)
 
--- pages/admin.py
+++ pages/admin.py
@@ -22,7 +22,7 @@ class FlatpageForm(forms.ModelForm):
 class FlatPageAdmin(admin.ModelAdmin):
     form = FlatpageForm
     fieldsets = (
-        (None, {'fields': ('url', 'title', 'content', 'sites')}),
+        (None, {'fields': ('url', 'title', 'content', 'sites', 'user', 'group', 'admingroup')}),
         #(_('Advanced options'), {'classes': ('collapse',), 'fields': ('enable_comments', 'registration_required', 'template_name')}),
     )
     list_display = ('url', 'title')
