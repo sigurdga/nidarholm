40a7df1cccb411fc21259a785b60c327c028d00d
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-12-11 14:13:18 +0100

    make usable admin list for UploadedFile

--- vault/admin.py
+++ vault/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from vault.models import UploadedFile
 
-admin.site.register(UploadedFile)
+class UploadedFileAdmin(admin.ModelAdmin):
+    list_display = ('filename', 'user', 'group')
+    list_filter = ('group', 'uploaded')
+    ordering = ('-uploaded',)
+    search_fields = ('filename', 'content_html')
+
+
+admin.site.register(UploadedFile, UploadedFileAdmin)
+
