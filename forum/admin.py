7558ccda65230d4d1964177005a7b1327f131c54
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-12-11 12:40:31 +0100

    make better admins for forum, news and organization

--- forum/admin.py
+++ forum/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from forum.models import Debate
 
-admin.site.register(Debate)
+class DebateAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    fields = ['parent', 'title', 'slug', 'content', 'user', 'group']
+    list_display = ('title', 'user', 'group', 'parent')
+    list_filter = ('group', 'created')
+    ordering = ('-created',)
+    search_fields = ('title', 'content_html')
+
+admin.site.register(Debate, DebateAdmin)
--- organization/admin.py
+++ organization/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from organization.models import GroupCategory, GroupProfile, Role, Membership, SiteProfile
 
+class SiteProfileAdmin(admin.ModelAdmin):
+    fields = ('group', 'site', 'contact_text')
+    list_display = ('site', 'group')
+admin.site.register(SiteProfile, SiteProfileAdmin)
+
 admin.site.register(GroupCategory)
 admin.site.register(GroupProfile)
 admin.site.register(Role)
 admin.site.register(Membership)
-admin.site.register(SiteProfile)
