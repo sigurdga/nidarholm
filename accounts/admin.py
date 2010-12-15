a5910e6ff842bba4d6c88c8be34b2375762f3f86
Sigurd Gartmann <sigurdga@sigurdga.no>
2010-12-16 00:17:41 +0100

    list more in user profile
    should merge this with user later

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from accounts.models import UserProfile
 
 class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user',)
-
+    list_display = ('user', 'status', 'born', 'joined')
+    ordering = ('user',)
+    list_filter = ('status',)
 admin.site.register(UserProfile, UserProfileAdmin)
+
