f5f87063a5ddb601b4ca5abd98fe9c013467dac2
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-01-16 17:48:25 +0100

    make list of members without category

--- organization/views.py
+++ organization/views.py
@@ -6,5 +6,6 @@ def group_category_list(request):
     return list_detail.object_list(request, GroupCategory.objects.all())
 
 def group_category_detail(request, slug, group_slug):
+    users_without_status = request.organization.group.user_set.filter(userprofile__status=None)
     return list_detail.object_detail(request, GroupCategory.objects.all(), slug=slug,
-            extra_context={'group_name': group_slug})
+            extra_context={'group_name': group_slug, 'users_without_status': users_without_status})
