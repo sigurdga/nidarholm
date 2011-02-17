1ddde688c11ad30927c5f26a98e621711eaf1c3f
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-02-17 22:13:01 +0100

    add new list of members of organization.group that does not get list emails

--- organization/views.py
+++ organization/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import list_detail
 from organization.models import GroupCategory
 from django.contrib.auth.models import User
+from django.db.models import Q
 from accounts.models import UserProfile
 
 
@@ -9,11 +10,12 @@ def group_category_list(request):
 
 def group_category_detail(request, slug, group_slug, status_id=None):
     users_without_status = request.organization.group.user_set.filter(userprofile__status=None).order_by('-id')
+    members_not_on_mailinglist = request.organization.group.user_set.filter(Q(userprofile__status=None)|Q(userprofile__status__gt=3)).order_by('-id')
     users_not_members = User.objects.filter(groups=None).order_by('-id')
     if not status_id:
         queryset = GroupCategory.objects.all()
         return list_detail.object_detail(request, queryset, slug=slug,
-            extra_context={'group_name': group_slug, 'users_without_status': users_without_status, 'users_not_members': users_not_members})
+            extra_context={'group_name': group_slug, 'users_without_status': users_without_status, 'users_not_members': users_not_members, 'members_not_on_mailinglist': members_not_on_mailinglist})
     else:
         if request.organization.group in request.user.groups.all():
             queryset = UserProfile.objects.filter(status=status_id).select_related('user').order_by('user__first_name', 'user__last_name')
