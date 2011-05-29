6dc0300a35b46ba649ff655e6cb62aa57c843cff
Sigurd Gartmann <sigurdga@sigurdga.no>
2011-05-29 13:11:13 +0200

    Update after forum views now is class based

--- navigation/templatetags/paginator.py
+++ navigation/templatetags/paginator.py
@@ -10,19 +10,20 @@ def paginator(context, adjacent_pages=4):
     last page links in addition to those created by the object_list generic
     view.
     """
-    page_numbers = [n for n in range(context["page"] - adjacent_pages, context["page"] + adjacent_pages + 1) if n > 0 and n <= context["pages"]]
+    paginator = context["paginator"]
+    page_obj = context["page_obj"]
+    page_numbers = [n for n in range(context["page"] - adjacent_pages, context["page"] + adjacent_pages + 1) if n > 0 and n <= paginator.num_pages]
     return {
-        "hits": context["hits"],
-        "results_per_page": context["results_per_page"],
+        "hits": paginator.count,
         "page": context["page"],
-        "pages": context["pages"],
+        "pages": paginator.num_pages,
         "page_numbers": page_numbers,
-        "next": context["next"],
-        "previous": context["previous"],
-        "has_next": context["has_next"],
-        "has_previous": context["has_previous"],
+        "next": page_obj.next_page_number,
+        "previous": page_obj.previous_page_number,
+        "has_next": page_obj.has_next,
+        "has_previous": page_obj.has_previous,
         "show_first": 1 not in page_numbers,
-        "show_last": context["pages"] not in page_numbers,
+        "show_last": paginator.num_pages not in page_numbers,
     }
 
 register.inclusion_tag("navigation/paginator.html", takes_context=True)(paginator)
