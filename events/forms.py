f150524e700c0a5853481696be715684c0f8d4a1
Sigurd Gartmann <sigurdga@sigurdga.no>
2012-02-21 10:53:52 +0100

    Make event form easier to use

--- events/forms.py
+++ events/forms.py
@@ -1,7 +1,9 @@
 from django.forms import ModelForm
+from samklang_utils.forms import MarkdownTextarea
 from events.models import Event
 
 class EventForm(ModelForm):
     class Meta:
         model = Event
         fields = ('title', 'content', 'event_category', 'start', 'end', 'whole_day')
+        widgets = {'content': MarkdownTextarea()}
